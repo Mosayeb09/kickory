37cdc57760b233f0bf8a218a49ddcddb5232a380
Mosayeb09 <mosayebhossain09@gmail.com>
2025-05-14 20:08:51 +0600

    feat: implement Home layout with Header and Footer components, update metadata, and enhance Header and Search functionality

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div>
-
+      <h1 className="h1-bold text-center p-10">Home</h1>
     </div>
   );
 }
--- src/components/shared/header/menu.tsx
+++ src/components/shared/header/menu.tsx
@@ -1,25 +1,46 @@
-import Link from "next/link";
+// import Link from "next/link";
 
-import { ShoppingCartIcon, UserIcon } from "lucide-react";
+// import { ShoppingCartIcon, UserIcon } from "lucide-react";
 
-const menu = () => {
-    return (
-        <div className="flex justify-end">
-            <nav className="flex gap-4 w-full">
-            <Link href={"/sign-in"} className="header-button">
-            <UserIcon className="h-8 w-8"></UserIcon>
-            <span className="font-bold">Sign in</span>
+// const menu = () => {
+//     return (
+//         <div className="flex justify-end">
+//             <nav className="flex gap-4 w-full">
+//             <Link href={"/sign-in"} className="header-button">
+//             <UserIcon className="h-8 w-8"></UserIcon>
+//             <span className="font-bold">Sign in</span>
 
             
-            </Link>
-            <Link href={"/cart"} className="header-button">
-            <ShoppingCartIcon className="h-8 w-8"></ShoppingCartIcon>
-            <span className="font-bold">Cart</span>
+//             </Link>
+//             <Link href={"/cart"} className="header-button">
+//             <ShoppingCartIcon className="h-8 w-8"></ShoppingCartIcon>
+//             <span className="font-bold">Cart</span>
           
-            </Link>
-            </nav>
-        </div>
-    );
+//             </Link>
+//             </nav>
+//         </div>
+//     );
+// };
+
+// export default menu;
+
+import Link from "next/link";
+import { ShoppingCartIcon, UserIcon } from "lucide-react";
+
+const Menu = () => {
+  return (
+    <nav className="flex justify-end gap-4 w-full">
+      <Link href="/sign-in" className="header-button flex items-center gap-2">
+        <UserIcon className="h-6 w-6" />
+        <span className="font-bold">Sign in</span>
+      </Link>
+
+      <Link href="/cart" className="header-button flex items-center gap-2">
+        <ShoppingCartIcon className="h-6 w-6" />
+        <span className="font-bold">Cart</span>
+      </Link>
+    </nav>
+  );
 };
 
-export default menu;
+export default Menu;
